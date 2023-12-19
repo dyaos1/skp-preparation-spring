29a9f5233c7096a34cf570891b7bc13c9c0d58de
Flzomb <spark3840@gmail.com>
2023-12-19 13:59:20 +0900

    feat: [#2]spring init
    gradle setup: swagger, kotest
    yml setup: mysql, h2

---
+++ skp-api-kotl/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "skp-api-kotl"
---
+++ skp-api-kotl/src/test/kotlin/com/example/skpapikotl/SkpApiKotlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.skpapikotl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkpApiKotlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
